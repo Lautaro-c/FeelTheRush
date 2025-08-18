5d3cdfb728c222a7f2460b73ef9d771395d4ad48
Lautaro-c <76079647+Lautaro-c@users.noreply.github.com>
2025-08-18 09:58:55 -0300

    Actors factory

---
+++ Assets/Scripts/ActorsFactory.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorsFactory : MonoBehaviour
+{
+    [SerializeField] private Actor[] actors;
+    private Dictionary<string, Actor> actorsDictionary;
+
+    private void Start()
+    {
+        actorsDictionary = new Dictionary<string, Actor>();
+        foreach (var actor in actors)
+        {
+            Debug.Log(actor.name);
+            actorsDictionary.Add(actor.name, actor);
+        }
+    }
+
+    public Actor Create(string name)
+    {
+        if(!actorsDictionary.TryGetValue(name, out Actor actor))
+        {
+            return null;
+        }
+        Debug.Log("Enemy instantiated");
+        return Instantiate(actor);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            Create("Enemy");
+        }
+    }
+}
